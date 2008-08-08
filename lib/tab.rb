910fec18e9dfb54b2e9c7a0f2ff184a54d347c78
Bodaniel Jeanes <me@bjeanes.com>
2008-08-08 07:22:33 -0700

    link optional

--- lib/tab.rb
+++ lib/tab.rb
@@ -5,9 +5,9 @@ module TabFu
       @list_id = list_id.to_s
     end
   
-    def method_missing(tab, name, options = {})
+    def method_missing(tab, name, link = '#' *options)
       active_class = (tab.to_s == current_tab.to_s) ? " active" : ""
-      text = active_class.blank? ? @context.link_to(name, options) : name
+      text = active_class.blank? ? @context.link_to(name, link, *options) : name
       "<li class=\"#{tab}#{active_class}\"><span><span>#{text}</span></span></li>"
     end
     

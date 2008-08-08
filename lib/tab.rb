36c39ff5a01ca1d14efd16b9e8b52b0114c9bcad
Bodaniel Jeanes <me@bjeanes.com>
2008-08-08 07:23:45 -0700

    fixed typo

--- lib/tab.rb
+++ lib/tab.rb
@@ -5,7 +5,7 @@ module TabFu
       @list_id = list_id.to_s
     end
   
-    def method_missing(tab, name, link = '#' *options)
+    def method_missing(tab, name, link = '#', *options)
       active_class = (tab.to_s == current_tab.to_s) ? " active" : ""
       text = active_class.blank? ? @context.link_to(name, link, *options) : name
       "<li class=\"#{tab}#{active_class}\"><span><span>#{text}</span></span></li>"
